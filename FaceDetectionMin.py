67dc2bc011eaecab9a37f420ae53ad941790a3ae
Alex <alexanderbraun@freenet.de>
2021-12-12 18:32:10 +0100

    change percentage text position

--- FaceDetectionMin.py
+++ FaceDetectionMin.py
@@ -9,7 +9,7 @@ pTime = 0
 
 mpFaceDetection = mp.solutions.mediapipe.python.solutions.face_detection
 mpDraw = mp.solutions.mediapipe.python.solutions.drawing_utils
-faceDetection = mpFaceDetection.FaceDetection(0.75)
+faceDetection = mpFaceDetection.FaceDetection(0.55)
 
 while True:
   # read the capture from webcam
@@ -23,7 +23,7 @@ while True:
       imageHeight, imageWidth, imageChannel = img.shape
       bbox = int(bboxC.xmin * imageWidth), int(bboxC.ymin * imageHeight), int(bboxC.width * imageWidth), int(bboxC.height * imageHeight)
       cv.rectangle(img, bbox, color=(255, 0, 255), thickness=2)
-      cv.putText(img, str(int(detection.score[0] * 100)) + '%', (int(bboxC.xmin * imageWidth), int(bboxC.ymin * imageHeight) - 20), cv.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 2)
+      cv.putText(img, str(int(detection.score[0] * 100)) + '%', (int(bboxC.xmin * imageWidth), int(bboxC.ymin * imageHeight) + int(bboxC.height * imageHeight) + 25), cv.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 2)
   cTime = time.time()
   fps = 1 / (cTime - pTime)
   pTime = cTime
